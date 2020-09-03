93e39c0542f7d6b9dca49df1949701baf331489c
Michael Cinus <michaelcinus@gmail.com>
2020-09-03 10:16:26 +0200

    correzione

--- index.php
+++ index.php
@@ -3,28 +3,28 @@
 
     $partite = [
 
-        1 => [
+        [
 
             "casa" => "Olimpia Milano",
             "ospite" => "Cantù",
             "punti-casa" => 55,
             "punti-ospite" => 60,
         ],
-        2 => [
+        [
 
             "casa" => "Treviso",
             "ospite" => "Trieste",
             "punti-casa" => 100,
             "punti-ospite" => 91,
         ],
-        3 => [
+        [
 
             "casa" => "Trento",
             "ospite" => "Venezia",
             "punti-casa" => 71,
             "punti-ospite" => 93,
         ],
-        4 => [
+        [
 
             "casa" => "Sassari",
             "ospite" => "Virtus",
@@ -34,7 +34,7 @@
         
     ];
 
-    for ($i=1; $i <= count($partite); $i++){
+    for ($i=0; $i <= count($partite)-1; $i++){
 
         echo $partite[$i]["casa"] . " - " . $partite[$i]["ospite"] . " | " . $partite[$i]["punti-casa"] . " - " . $partite[$i]["punti-ospite"];
         echo "<br>";
